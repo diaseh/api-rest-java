2bfc126517670abc2698ca1b6ebff36fa915da4e
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-07-29 00:07:25 -0300

    Atualizações e Conexão Banco de dados

--- src/main/java/med/voll/api/medico/DadosCadastroMedicos.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedicos.java
@@ -1,6 +1,8 @@
 package med.voll.api.medico;
 
 import med.voll.api.endereco.DadosEndereco;
+/* Aqui é um DTO (Data Transfer Object)
+*/
 
-public record DadosCadastroMedicos(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedicos(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco ) {
 }
